37c774de276e1089e70dc16e5a38a2c47a819d75
[Jeremy Ray] <jeremydray@gmail.com>
2024-07-16 23:07:57 -0500

    Added API call for Ticketmaster and output loop

--- assets/js/searchresults-script.js
+++ assets/js/searchresults-script.js
@@ -8,17 +8,38 @@
 //Get event images
 //https://app.ticketmaster.com/discovery/v2/events/k7vGFKzleBdwS/images.json?apikey=EGPaeJvEARHDtDujZj2YKE5LyNqeJry5
 
-function getVenueData(city) {
+async function getVenueData(city) {
 
-    const zipSearch = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&radius=100&size=30&classificationName=music&apikey=EGPaeJvEARHDtDujZj2YKE5LyNqeJry5`;
-    fetch(zipSearch)
-        .then(function (response) {
-            console.log(response.json());
-        })
+    const citySearchUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&radius=100&size=30&classificationName=music&apikey=EGPaeJvEARHDtDujZj2YKE5LyNqeJry5`;
+    const eventInfo = await fetch(citySearchUrl)
+    const eventData = await eventInfo.json();
+    return eventData._embedded.events
+    // .then(function (response) {
+    //     console.log(response.json());
+    //     // displayVenueData(response)
+    // })
 }
 
-console.log(getVenueData('Dallas'));
+getVenueData('Dallas').then(function (events) {
+    console.log(events);
+    for (let i = 0; i < 3; i++) { //change to events length
+        console.log(events[i].name);
+        console.log(events[i].dates.start.localDate);
+        console.log(events[i].url);
+        console.log(events[i]._embedded.venues[0].location.latitude);
+        console.log(events[i]._embedded.venues[0].location.longitude);
+    }
+})
 
 
 
+// console.log(test)
+
+
+// function displayVenueData(responseObj) {
+//     let response {
+//         venueName: responseObj._embedded.events[0].name,
+//     }
+// }
+// console.log(responseData.name)
 
